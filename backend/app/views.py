c368e50536559fbcee9a21f9265314249b2c7611
tojocorm <63367603+tojocorm@users.noreply.github.com>
2022-02-23 14:45:11 -0500

    stuff

--- backend/app/views.py
+++ backend/app/views.py
@@ -6,20 +6,52 @@ from django.db import connection
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+import os, time
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+@csrf_exempt
 def findfaces(request):
-    if request.method != 'GET':
-        return HttpResponse(status=404)
-    response = {}
-    response['chatts'] = ['Replace Me', 'DUMMY RESPONSE'] # **DUMMY response!**
-    return JsonResponse(response)
+    if request.method != 'POST':
+        return HttpResponse(status=400)
 
+    # loading form-encoded data
+    username = request.POST.get("userid")
 
+    if request.FILES.get("image"):
+        content = request.FILES['image']
+        filename = username+str(time.time())+".jpeg"
+        fs = FileSystemStorage()
+        filename = fs.save(filename, content)
+        imageurl = fs.url(filename)
+    else:
+        imageurl = None
+
+    return JsonResponse({})
+
+@csrf_exempt
 def findactor(request):
-    if request.method != 'GET':
-        return HttpResponse(status=404)
-    response = {}
-    response['chatts'] = ['Replace Me', 'DUMMY RESPONSE'] # **DUMMY response!**
-    return JsonResponse(response)
+    if request.method != 'POST':
+        return HttpResponse(status=400)
+
+    # loading form-encoded data
+    userid = request.POST.get("userid")
+
+    if request.FILES.get("image"):
+        content = request.FILES['image']
+        filename = userid+str(time.time())+".jpeg"
+        fs = FileSystemStorage()
+        filename = fs.save(filename, content)
+        imageurl = fs.url(filename)
+    else:
+        imageurl = None
+
+    actorName = ""
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO history (userid, actor, imageurl) VALUES '
+                   '(%s, %s, %s);', (userid, actorName, imageurl))
+
+    return JsonResponse({})
 
 
 def getactorinfo(request):
--- backend/routing/settings.py
+++ backend/routing/settings.py
@@ -125,3 +125,7 @@ STATIC_ROOT = BASE_DIR/'static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'https://3.144.236.126/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
