048b96e15c5756e7413b7ab3e60c0721bd1abaff
jackrallo <jrallo@umich.edu>
2022-03-12 16:30:08 -0500

    Frontend Created

---
+++ frontend/ActorCapture/ActorCapture/ActorCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActorCaptureApp.swift
+//  ActorCapture
+//
+//  Created by Jack  Rallo  on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ActorCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/ActorCapture/ActorCapture/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ActorCapture
+//
+//  Created by Jack  Rallo  on 3/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+/*
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+ */
